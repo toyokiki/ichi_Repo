81a3998fb1c358c1d5d1f161ac9b642179749d0a
Yoshi Ichiki <itiki@toyo-kiki.co.jp>
2016-05-16 22:05:43 +0900

    A first time commit

---
+++ sale_check_order/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+{
+	"name": "Sale Check Order",
+	"summary": "check whether order from customer exists",
+	"version": "9.0.0.1.1",
+	"category": "Sales",
+	"website": "http://www.toyo-kiki.co.jp",
+	"author": "ichi",
+	"license": "AGPL-3",
+	"application": False,
+	"installable": True,
+	"depends": [
+		"sale",
+	],
+	"data": [
+		"views/sale_view.xml",
+	],
+}
