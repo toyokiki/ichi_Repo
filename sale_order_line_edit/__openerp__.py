db10488b54d4fdab34b9db2285b43d4adf706e71
Yoshi Ichiki <itiki@toyo-kiki.co.jp>
2016-05-18 12:04:35 +0900

    add sale_order_line_edit

---
+++ sale_order_line_edit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+{
+	"name": "Sale Order Line Menu",
+	"summary": "add menu for order line",
+	"version": "9.0.0.1.1",
+	"category": "Sales",
+	"website": "http://www.toyo-kiki.co.jp",
+	"author": "ichi",
+	"license": "AGPL-3",
+	"application": False,
+	"installable": True,
+	"depends": [
+		"sale",
+	],
+	"data": [
+		"views/sale_view.xml",
+	],
+}
