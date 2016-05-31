b91236cd6ccd0cc744551d0b89c530de4f86fd33
ichi23de5 <ichi2266@nike.eonet.ne.jp>
2016-05-31 14:53:11 +0800

    Adding sale_order_plan

---
+++ sale_order_plan/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016 Rooms For (Hong Kong) Limited T/A OSCG
+#    <https://www.odoo-asia.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    "name": "Plan_Sale_Order ",
+    "summary": "add Plan and CompleteDay to Sale Order",
+    "version": "9.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.toyo-kiki.co.jp/",
+    "author": "TOYOKIKI INC.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_list_view.xml",
+        "views/sale_form_view.xml",
+    ],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sale_order_plan/models/sale.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright TOYOKIKI INC.
+#    <https://www.toyo-kiki.co.jp>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from openerp import models, fields
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    plan_id = fields.Many2one(string="Plan Name", help="Plan Name for Quotation", relation='sale.order')
+    complete_date = fields.Date('Complete Date')
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
